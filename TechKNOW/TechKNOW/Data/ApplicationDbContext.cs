f36cf9be1c56c95700c7d442adaec478da4ff8c1
SamzFerg <SamzFerg@gmail.com>
2018-08-29 10:26:32 -0500

    Added Repair View Files and Added RepairEntity to Database

--- TechKNOW/TechKNOW/Data/ApplicationDbContext.cs
+++ TechKNOW/TechKNOW/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TechKNOW.Models.EntityModel;
 
 namespace TechKNOW.Data
 {
@@ -12,5 +13,7 @@ namespace TechKNOW.Data
             : base(options)
         {
         }
+
+        public DbSet<RepairEntity> Repairs { get; set; }
     }
 }
---
+++ TechKNOW/TechKNOW/Data/Migrations/20180829150225_AddedRepairList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechKNOW.Data.Migrations
+{
+    public partial class AddedRepairList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateTable(
+                name: "ChromebookModel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AssetTag = table.Column<string>(nullable: true),
+                    SerialNumber = table.Column<string>(nullable: true),
+                    IsLoaner = table.Column<bool>(nullable: false),
+                    StudentID = table.Column<int>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChromebookModel", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repairs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TechnicianID = table.Column<int>(nullable: false),
+                    StudentID = table.Column<int>(nullable: false),
+                    ChromebookID = table.Column<int>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Comments = table.Column<string>(nullable: true),
+                    RepairCost = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repairs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Repairs_ChromebookModel_ChromebookID",
+                        column: x => x.ChromebookID,
+                        principalTable: "ChromebookModel",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PartModel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PartNumber = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    RepairEntityID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartModel", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PartModel_Repairs_RepairEntityID",
+                        column: x => x.RepairEntityID,
+                        principalTable: "Repairs",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartModel_RepairEntityID",
+                table: "PartModel",
+                column: "RepairEntityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_ChromebookID",
+                table: "Repairs",
+                column: "ChromebookID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PartModel");
+
+            migrationBuilder.DropTable(
+                name: "Repairs");
+
+            migrationBuilder.DropTable(
+                name: "ChromebookModel");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
